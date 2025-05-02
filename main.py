f08a410dfe67757e209bc0ea8fcbd65f4040f2c6
AlonsoCod3 <codelonso@gmail.com>
2025-05-02 17:03:57 +0000

    Añadiendo metodo options

--- main.py
+++ main.py
@@ -21,5 +21,16 @@ def get_external_data(id):
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)}), 500
 
+
+@app.route('/reniec/<id>', methods=['OPTIONS'])
+    def options():
+      response = make_response()
+      response.headers.add('Access-Control-Allow-Origin', '*') # Permite todos los orígenes
+      response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+      return response
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
