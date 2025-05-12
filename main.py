a59079e3c7fbf09979a20341f9b70a5c44ac15d2
AlonsoCod3 <codelonso@gmail.com>
2025-05-12 14:45:00 +0000

    Probando añadido a json virtual

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 import requests
+from products import get_all, get_by_id, new_product, edit_product, delete_product
 
 app = Flask(__name__)
 CORS(app)  # Habilita CORS para todas las rutas
@@ -12,6 +13,41 @@ token = os.getenv("TOKEN")
 @app.route('/', methods=['GET'])
 def index():
   return "HOLA"
+
+@app.route('/products')
+def productos():
+  return jsonify(get_all())
+
+@app.route('/products/new')
+def newo():
+    nuevo = {
+        "id": 1,
+        "code": "0002",
+        "name": "apanado",
+        "description": "Plato de apanado personal",
+        "price": 25.00,
+        "variants": {}
+    }
+    return jsonify(new_product(nuevo))
+
+@app.route('/products/new/<id>')
+def newoid(id):
+    return jsonify(get_by_id(id))
+
+@app.route('/products/del/<id>')
+def delo(id):
+    delete_product(id)
+    return jsonify(get_all())
+
+@app.route('/products/edit/<id>')
+def edito(id):
+    atu = {
+        "name": "cevichazo",
+        "price": 10.00
+    }
+    edit_product(id, atu)
+    return jsonify(get_all())
+
 
 # /reniec/dni?numero=46027897
 @app.route('/reniec/<id>', methods=['GET'])
