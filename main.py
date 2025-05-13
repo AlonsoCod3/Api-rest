a0273958a65663ee2be054f0fe0165fe733bb6e1
AlonsoCod3 <codelonso@gmail.com>
2025-05-13 02:38:13 +0000

    Actualizacion para solo productos

--- main.py
+++ main.py
@@ -1,66 +1,16 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
-import requests
 from products.routes import routing
 
 app = Flask(__name__)
 CORS(app)  # Habilita CORS para todas las rutas
 
-url = os.getenv("url")
-token = os.getenv("TOKEN")
-
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
   return "HOLA"
 
 app.register_blueprint(routing, url_prefix="/productos")
-
-# /reniec/dni?numero=46027897
-@app.route('/reniec/<id>', methods=['GET'])
-def dni(id):
-    try:
-        header = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {token}"
-        }
-        response = requests.get(f'{url}/reniec/dni?numero={id}', headers = header)
-        response.raise_for_status()  # Raises HTTPError for bad responses (4xx or 5xx)
-        data = response.json()
-        return jsonify(data), 200
-    except requests.exceptions.RequestException as e:
-        return jsonify({'error': str(e)}), 500
-
-# /sunat/ruc?numero=<ruc>
-@app.route('/sunat/<id>', methods=['GET'])
-def ruc(id):
-    try:
-        header = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {token}"
-        }
-        response = requests.get(f'{url}/sunat/ruc?numero={id}', headers = header)
-        response.raise_for_status()  # Raises HTTPError for bad responses (4xx or 5xx)
-        data = response.json()
-        return jsonify(data), 200
-    except requests.exceptions.RequestException as e:
-        return jsonify({'error': str(e)}), 500
-
-# /sunat/ruc/full?numero=<ruc>
-@app.route('/full/sunat/<id>', methods=['GET'])
-def ruc_full(id):
-    try:
-        header = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {token}"
-        }
-        response = requests.get(f'{url}/sunat/ruc/full?numero={id}', headers = header)
-        response.raise_for_status()  # Raises HTTPError for bad responses (4xx or 5xx)
-        data = response.json()
-        return jsonify(data), 200
-    except requests.exceptions.RequestException as e:
-        return jsonify({'error': str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
