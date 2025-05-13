0cbd0f3e0164e591af69e45c2419feafdbf331e5
AlonsoCod3 <codelonso@gmail.com>
2025-05-13 02:08:59 +0000

    Configuracion inical

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 import requests
-from products import get_all, get_by_id, new_product, edit_product, delete_product
+from products.routes import routing
 
 app = Flask(__name__)
 CORS(app)  # Habilita CORS para todas las rutas
@@ -14,40 +14,7 @@ token = os.getenv("TOKEN")
 def index():
   return "HOLA"
 
-@app.route('/products')
-def productos():
-  return jsonify(get_all())
-
-@app.route('/products/new')
-def newo():
-    nuevo = {
-        "id": 1,
-        "code": "0002",
-        "name": "apanado",
-        "description": "Plato de apanado personal",
-        "price": 25.00,
-        "variants": {}
-    }
-    return jsonify(new_product(nuevo))
-
-@app.route('/products/new/<id>')
-def newoid(id):
-    return jsonify(get_by_id(id))
-
-@app.route('/products/del/<id>')
-def delo(id):
-    delete_product(id)
-    return jsonify(get_all())
-
-@app.route('/products/edit/<id>')
-def edito(id):
-    atu = {
-        "name": "cevichazo",
-        "price": 10.00
-    }
-    edit_product(id, atu)
-    return jsonify(get_all())
-
+app.register_blueprint(routing, url_prefix="/productos")
 
 # /reniec/dni?numero=46027897
 @app.route('/reniec/<id>', methods=['GET'])
@@ -60,7 +27,7 @@ def dni(id):
         response = requests.get(f'{url}/reniec/dni?numero={id}', headers = header)
         response.raise_for_status()  # Raises HTTPError for bad responses (4xx or 5xx)
         data = response.json()
-        return jsonify(data)
+        return jsonify(data), 200
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)}), 500
 
@@ -75,7 +42,7 @@ def ruc(id):
         response = requests.get(f'{url}/sunat/ruc?numero={id}', headers = header)
         response.raise_for_status()  # Raises HTTPError for bad responses (4xx or 5xx)
         data = response.json()
-        return jsonify(data)
+        return jsonify(data), 200
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)}), 500
 
@@ -90,7 +57,7 @@ def ruc_full(id):
         response = requests.get(f'{url}/sunat/ruc/full?numero={id}', headers = header)
         response.raise_for_status()  # Raises HTTPError for bad responses (4xx or 5xx)
         data = response.json()
-        return jsonify(data)
+        return jsonify(data), 200
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)}), 500
 
